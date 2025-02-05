a25883ca449c292c2d65a49ead7b42daa2b619d6
Kalev <kalev.suik@gmail.com>
2025-02-05 11:39:02 +0200

    gpt-4o-mini

--- src/main/java/guru/springframework/springaiintro/services/OpenAIServiceImpl.java
+++ src/main/java/guru/springframework/springaiintro/services/OpenAIServiceImpl.java
@@ -5,6 +5,7 @@ import guru.springframework.springaiintro.model.GetCapitalRequest;
 import guru.springframework.springaiintro.model.Question;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.ChatOptions;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.beans.factory.annotation.Value;
@@ -31,7 +32,7 @@ public class OpenAIServiceImpl implements OpenAIService {
     @Override
     public Answer getCapital(GetCapitalRequest getCapitalRequest) {
         PromptTemplate promptTemplate = new PromptTemplate(getCapitalPrompt);
-        Prompt prompt = promptTemplate.create(Map.of("stateOrCountry", getCapitalRequest.stateOrCountry()));
+        Prompt prompt = promptTemplate.create(Map.of("stateOrCountry", getCapitalRequest.stateOrCountry()), ChatOptions.builder().model("gpt-4o-mini").build());
         ChatResponse response = chatModel.call(prompt);
 
         return new Answer(response.getResult().getOutput().getContent());
@@ -42,7 +43,7 @@ public class OpenAIServiceImpl implements OpenAIService {
         System.out.println("I was called");
 
         PromptTemplate promptTemplate = new PromptTemplate(question.question());
-        Prompt prompt = promptTemplate.create();
+        Prompt prompt = promptTemplate.create(ChatOptions.builder().model("gpt-4o-mini").build());
         ChatResponse response = chatModel.call(prompt);
 
         return new Answer(response.getResult().getOutput().getContent());
@@ -51,7 +52,7 @@ public class OpenAIServiceImpl implements OpenAIService {
     @Override
     public String getAnswer(String question) {
         PromptTemplate promptTemplate = new PromptTemplate(question);
-        Prompt prompt = promptTemplate.create();
+        Prompt prompt = promptTemplate.create(ChatOptions.builder().model("gpt-4o-mini").build());
         ChatResponse response = chatModel.call(prompt);
 
         return response.getResult().getOutput().getContent();
